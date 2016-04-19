6c67553368c08e78a74f94e186b41f229a22ae90
Steve Pentler <stevepentler@gmail.com>
2016-04-18 22:08:38 -0600

    Sends messages to every client

--- public/client.js
+++ public/client.js
@@ -1,1 +1,7 @@
 const socket = io();
+
+let connectionCount = document.getElementById('connection-count');
+
+socket.on('usersConnected', function (count) {
+  connectionCount.innerText = 'Connected Users: ' + count;
+});
